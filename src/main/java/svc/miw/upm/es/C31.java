eee9a1cac1d44ffb9fac1e7a6b8f4b217fe49892
Moons <Moons@DESKTOP-OVVFGM0>
2017-10-15 13:33:12 -0500

    C31

---
+++ src/main/java/svc/miw/upm/es/C31.java
@@ -0,0 +1,19 @@
+package svc.miw.upm.es;
+
+public class C31 {
+
+	public C31() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String m1() {
+		return "m1";
+
+	}
+
+	public static String m2() {
+		return "m2";
+
+	}
+
+}
