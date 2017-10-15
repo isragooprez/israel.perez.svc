d53dd409eb1a8ee706c2bacc0bbdebe87b1c99ad
Moons <Moons@DESKTOP-OVVFGM0>
2017-10-15 12:48:48 -0500

    C21

---
+++ src/main/java/svc/miw/upm/es/C21.java
@@ -0,0 +1,17 @@
+package svc.miw.upm.es;
+
+public class C21 {
+
+	public C21() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String m1() {
+		return "m1";
+	}
+
+	public String m2() {
+		return "m2";
+	}
+
+}
