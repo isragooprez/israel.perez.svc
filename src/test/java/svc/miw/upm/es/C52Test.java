8fa836727193706cfbb6818cf59e6fe9f7f5b10a
Moons <Moons@DESKTOP-OVVFGM0>
2017-10-16 17:33:28 -0500

    C52Test

---
+++ src/test/java/svc/miw/upm/es/C52Test.java
@@ -0,0 +1,21 @@
+package svc.miw.upm.es;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C52Test {
+
+	@Before
+	public void before() {
+		new C52();
+	}
+
+	@Test
+	public void testmA() {
+		assertEquals("mA", C52.mA());
+
+	}
+
+}
